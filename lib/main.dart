b17d242b5c948fb27e78e4d65f60c3fb535991fe
Ridzz05 <muhrizkialghipari@gmail.com>
2025-10-20 12:45:49 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
-      home: DetailScreen(candi: candiListList[0]),
     );
   }
 }
