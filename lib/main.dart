06ad4586a3c8de0335fbfe957e1f157d123daf2c
Ridzz05 <muhrizkialghipari@gmail.com>
2025-11-13 16:18:40 +0700

    list view done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:wisata_candi_mrizki_algipari/screens/search_screen.dart';
 import '/data/candi_data.dart';
 import '/screens/detail_screen.dart';
 import '/screens/signin_screen.dart';
+import '/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi_mrizki_algipari/models/candi.dart';
+import '/models/candi.dart';
+import '/data/candi_data.dart';
 
 
 class SearchScreen extends StatefulWidget{
@@ -10,40 +11,87 @@ class SearchScreen extends StatefulWidget{
 }
 
 class _SearchScreenState extends State<SearchScreen>{
-  @override
   //TODO 1: Deklarasikan variabel yang dibutuhkan 
-  List<Candi> _filteredCandi = [];
-  String _searchQuery = "";
-  final TextEditingController _searchController = TextEditingController();
+  late List<Candi> _filteredCandi;
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredCandi = candiList;
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
       // TODO 2: Buat appbar dengan judul pencarian candi
       appBar: AppBar(title: Text('Pencarian Candi'),),
       // TODO 3: Buat Body Berupa Column
-      body: Column(children: [
-      TextField(
-        controller: _searchController,
-        decoration: const InputDecoration(
-          hintText: 'Cari candi',
-          prefixIcon: Icon(Icons.search),
-          border: OutlineInputBorder(),
-        ),
-        onChanged: (value) {
-          setState(() {
-            _searchQuery = value;
-          });
-        },
-      ),
-      // TODO 5: Buat ListView menampilkan hasil pencarian sebagai anak dari column
-      ListView.builder(
-        itemCount: _filteredCandi.length,
-        itemBuilder: (context, index){
-          final candi = candi _filteredCandi[index];
-          return Card();
-        }
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.deepPurple[50],
+              ),
+              child: TextField(
+                autofocus: false,
+                decoration: InputDecoration(
+                  hintText: 'Cari Candi...',
+                  prefixIcon: const Icon(Icons.search),
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                ),
+              ),
+            ),
+          ),
+          // TODO 5: Buat ListView Candi sebagai anak dari column
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Expanded(
+              child: ListView.builder(
+                itemCount: _filteredCandi.length,
+                itemBuilder: (context, index) {
+                  final candi = _filteredCandi[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          width: 100,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              candi.imageAsset,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                candi.name,
+                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(candi.location),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       )
-      ],)
     );
   }
 }
