5bbdfd47ea21228bf41f6ca701bb1cd0a3eee985
Ridzz05 <muhrizkialghipari@gmail.com>
2025-12-15 16:21:10 +0700

    update  fungsi sign in dan signup

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:image_picker/image_picker.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -33,7 +34,7 @@ class _ProfileScreenState extends State<ProfileScreen>
     await prefs.setBool('isSignedIn', false);
 
     setState(() {
-      isSignedIn = !isSignedIn;
+      isSignedIn = false;
       userName = '';
       fullName = '';
       favoriteCandiCount = 0;
@@ -56,10 +57,40 @@ class _ProfileScreenState extends State<ProfileScreen>
 
   void _identitas() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      fullName = prefs.getString("fullname") ?? "";
-      userName = prefs.getString("username") ?? "";
-    });
+    final encryptedFullName = prefs.getString("fullname");
+    final encryptedUserName = prefs.getString("username");
+    final keyString = prefs.getString('key');
+    final ivString = prefs.getString('iv');
+
+    if (encryptedFullName != null &&
+        encryptedUserName != null &&
+        keyString != null &&
+        ivString != null &&
+        encryptedFullName.isNotEmpty &&
+        encryptedUserName.isNotEmpty &&
+        keyString.isNotEmpty &&
+        ivString.isNotEmpty) {
+      try {
+        final key = encrypt.Key.fromBase64(keyString);
+        final iv = encrypt.IV.fromBase64(ivString);
+        final encrypter = encrypt.Encrypter(encrypt.AES(key));
+
+        setState(() {
+          fullName = encrypter.decrypt64(encryptedFullName, iv: iv);
+          userName = encrypter.decrypt64(encryptedUserName, iv: iv);
+        });
+      } catch (_) {
+        setState(() {
+          fullName = "";
+          userName = "";
+        });
+      }
+    } else {
+      setState(() {
+        fullName = "";
+        userName = "";
+      });
+    }
   }
 
   // Fungsi untuk mendapatkan jumlah favorit
